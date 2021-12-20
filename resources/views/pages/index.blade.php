ac03869d2adf011dd787088a2dd3ac4aaedf211e
Hsueh <40841121@gm.nfu.edu.tw>
2021-12-20 15:30:48 +0800

    對 index 頁面套用bootstrap進行美化

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,21 +1,30 @@
 @extends('layouts.master')
 @section('content')
-    <table class="table">
-        <thead>
-        <tr>
-            <td>標題</td>
-            <td>內容</td>
-            <td>備註</td>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($data as $row)
+    <div class="container p-3">
+        <table class="table">
+            <thead>
             <tr>
-                <td>{{$row->title}}</td>
-                <td>{{$row->content}}</td>
-                <td>{{$row->remark}}</td>
+                <td>標題</td>
+                <td>內容</td>
+                <td>備註</td>
+                <td>功能</td>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @foreach($data as $row)
+                <tr>
+                    <td class="col-sm-3">{{$row->title}}</td>
+                    <td class="col-sm-3">{{$row->content}}</td>
+                    <td class="col-sm-3">{{$row->remark}}</td>
+                    <td class="col-sm-3">
+                        <button class="btn btn-outline-success">修改</button>
+                        <button class="btn btn-outline-danger"
+                                onclick=window.location.href={{route('delete')}}?delete_id={{$row->id}}>刪除</button>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
 @endsection
